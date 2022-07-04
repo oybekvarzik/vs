3c8f51ca8a1d6c7c29c6d53c676ea126aa26c84d
oybekvarzik <104968044+oybekvarzik@users.noreply.github.com>
2022-07-04 17:27:04 +0500

    vc

---
+++ 1_lesson.js
@@ -0,0 +1,11 @@
+let array = [1,2,3,4,5];
+array.push(6)
+array.unshift(0)
+array.splice(0,2, 4,5)
+console.log(array);
+console.log(array.length);
+
+
+let arraySecond = ['a','b','c', 'd'];
+console.log(arraySecond.join(''));
+
